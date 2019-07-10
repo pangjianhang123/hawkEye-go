e6fc4396615176d86db25625b2bcbbfdcb4be295
张睿 <zhangr@tiup.us>
2019-07-10 16:44:52 +0800

    初始化项目目录结构

---
+++ nest/api_server/api.go
@@ -0,0 +1,22 @@
+package api_server
+
+import(
+	//"github.com/apsdehal/go-logger"
+	"github.com/gin-gonic/gin"
+)
+
+func SaveJob(ctx *gin.Context){
+
+}
+
+func AddJob(ctx *gin.Context){
+
+}
+
+func DeleteJob(ctx *gin.Context){
+
+}
+
+func KillJob(ctx *gin.Context){
+
+}
