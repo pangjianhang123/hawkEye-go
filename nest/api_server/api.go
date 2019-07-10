d7a2038c451eb05465fab3ac352e16932e9b48c5
ricky <zradelaide1122@gmail.com>
2019-07-10 20:49:36 +0800

    a

--- nest/api_server/api.go
+++ nest/api_server/api.go
@@ -3,7 +3,16 @@ package api_server
 import(
 	//"github.com/apsdehal/go-logger"
 	"github.com/gin-gonic/gin"
+
+	"go.etcd.io/etcd/clientv3"
 )
+
+
+type JobMgr struct {
+	client *clientv3.Client
+	kv clientv3.KV
+	lease clientv3.Lease
+}
 
 func SaveJob(ctx *gin.Context){
 
